797635facb5d45e72ba471c51e9c11e690acd076
Jabepi <javier.beiro@hotmail.com>
2024-10-28 17:30:05 +0100

    question 9 working

--- lab4/pcg.c
+++ lab4/pcg.c
@@ -12,8 +12,12 @@ float time_in_SSOR = 0;
 double dot(int n, const double* x, const double* y)
 {
     double result = 0;
+
+    // Parallelize the loop with OpenMP using reduction on 'result'
+    #pragma omp parallel for reduction(+:result)
     for (int i = 0; i < n; ++i)
         result += x[i]*y[i];
+
     return result;
 }
 
@@ -59,19 +63,26 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
     tic(0);
 
     /* Form residual */
-    // tic(1);
+    tic(3);
     Afun(n, Adata, r, x);
-    // double timeAfun = toc(1);
+    double timeAfun = toc(3);
+    double timeDot = 0;
+    double timeMfun = 0;
 
     for (int i = 0; i < n; ++i) r[i] = b[i]-r[i];
 
     for (step = 0; step < maxit && !is_converged; ++step) {
+
+        tic(5);
         Mfun(n, Mdata, z, r);
+        timeMfun += toc(5);
+        
         rho_prev = rho;
         
-        // tic(1);
+        tic(4);
         rho = dot(n, r, z);
-        // time_dotproduct += toc(1);
+        toc(4);
+        timeDot += toc(4);
 
         if (step == 0) {
             rho0 = rho;
@@ -80,15 +91,16 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
             double beta = rho/rho_prev;
             for (int i = 0; i < n; ++i) p[i] = z[i] + beta*p[i];
         }
-        // tic(1);
+        tic(3);
         Afun(n, Adata, q, p);
-        // timeAfun += toc(1);
+        timeAfun += toc(3);
 
-        // tic(1);
-        double dotAux = dot(n, p, q);
-        // time_dotproduct += toc(1);
+        tic(4);
+        double dotProduct = dot(n, p, q); 
+        toc(4);
+        timeDot += toc(4);
 
-        double alpha = rho/dotAux;
+        double alpha = rho/dotProduct;
         for (int i = 0; i < n; ++i) x[i] += alpha*p[i];
         for (int i = 0; i < n; ++i) r[i] -= alpha*q[i];
         is_converged = (rho/rho0 < rtol2);
@@ -99,8 +111,11 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
             sqrt(rho/rho0), is_converged ? "<=" : ">", rtol, toc(0),
             time_in_poisson, time_in_SSOR);
 
-    // printf("Time in Afun: %g seconds\n", timeAfun);
-    // printf("Time in dotproduct: %g seconds\n", time_dotproduct);
+    printf("Time in Afun: %g seconds\n", timeAfun);
+    printf("Time in dot: %g seconds\n", timeDot);
+    printf("Time in Mfun: %g seconds\n", timeMfun);
+
+
     free(p);
     free(q);
     free(z);
