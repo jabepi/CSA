edd0853046f3efb3080011423c152ee026bc293e
Jabepi <javier.beiro@hotmail.com>
2024-10-28 17:31:32 +0100

    organiced structure after question 9

--- lab4/pcg.c
+++ lab4/pcg.c
@@ -12,12 +12,8 @@ float time_in_SSOR = 0;
 double dot(int n, const double* x, const double* y)
 {
     double result = 0;
-
-    // Parallelize the loop with OpenMP using reduction on 'result'
-    #pragma omp parallel for reduction(+:result)
     for (int i = 0; i < n; ++i)
         result += x[i]*y[i];
-
     return result;
 }
 
@@ -63,26 +59,15 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
     tic(0);
 
     /* Form residual */
-    tic(3);
     Afun(n, Adata, r, x);
-    double timeAfun = toc(3);
-    double timeDot = 0;
-    double timeMfun = 0;
 
     for (int i = 0; i < n; ++i) r[i] = b[i]-r[i];
 
     for (step = 0; step < maxit && !is_converged; ++step) {
-
-        tic(5);
         Mfun(n, Mdata, z, r);
-        timeMfun += toc(5);
-        
         rho_prev = rho;
         
-        tic(4);
         rho = dot(n, r, z);
-        toc(4);
-        timeDot += toc(4);
 
         if (step == 0) {
             rho0 = rho;
@@ -91,16 +76,9 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
             double beta = rho/rho_prev;
             for (int i = 0; i < n; ++i) p[i] = z[i] + beta*p[i];
         }
-        tic(3);
         Afun(n, Adata, q, p);
-        timeAfun += toc(3);
 
-        tic(4);
-        double dotProduct = dot(n, p, q); 
-        toc(4);
-        timeDot += toc(4);
-
-        double alpha = rho/dotProduct;
+        double alpha = rho/dot(n, p, q);
         for (int i = 0; i < n; ++i) x[i] += alpha*p[i];
         for (int i = 0; i < n; ++i) r[i] -= alpha*q[i];
         is_converged = (rho/rho0 < rtol2);
@@ -110,12 +88,6 @@ double pcg(int n, mul_fun_t Mfun, void* Mdata, mul_fun_t Afun, void* Adata,
            "Time in Poisson: %g seconds\nTime in SSOR: %g seconds\n", step,
             sqrt(rho/rho0), is_converged ? "<=" : ">", rtol, toc(0),
             time_in_poisson, time_in_SSOR);
-
-    printf("Time in Afun: %g seconds\n", timeAfun);
-    printf("Time in dot: %g seconds\n", timeDot);
-    printf("Time in Mfun: %g seconds\n", timeMfun);
-
-
     free(p);
     free(q);
     free(z);
